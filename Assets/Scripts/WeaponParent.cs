50ce8aa2cbe00d43f246cc2d4e792c06104a3792
justinherring <jmherring4747@gmail.com>
2023-02-25 13:54:42 -0600

    weapon flips

--- Assets/Scripts/WeaponParent.cs
+++ Assets/Scripts/WeaponParent.cs
@@ -8,7 +8,20 @@ public class WeaponParent : MonoBehaviour
 
     private void Update()
     {
+        Vector2 direction = (Pointerposition - (Vector2)transform.position).normalized;
+
         // this rotates the actual weapon
-        transform.right = (Pointerposition - (Vector2)transform.position).normalized;
+        transform.right = direction;
+
+        Vector2 scale = transform.localScale;
+        if(direction.x < 0)
+        {
+            scale.y = -1;
+        } 
+        else
+        {
+            scale.y = 1;
+        }
+        transform.localScale = scale;
     }
 }
