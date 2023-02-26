5a7fa8a916b5e8b339dd9f04044b2fb642e4f22c
justinherring <jmherring4747@gmail.com>
2023-02-26 12:46:44 -0600

    finishing touches

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -13,6 +13,9 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField]
     private int gameOverScene = 2;
+
+    [SerializeField]
+    private int victoryScene = 3;
     public void GotoMainMenu()
     {
         SceneManager.LoadScene(gameMenuScene);
@@ -26,6 +29,11 @@ public class MenuManager : MonoBehaviour
     public void GotoGameOver()
     {
         SceneManager.LoadScene(gameOverScene);
+    }
+
+    public void GotoVictory()
+    {
+        SceneManager.LoadScene(victoryScene);
     }
 
     public void QuitGame()
